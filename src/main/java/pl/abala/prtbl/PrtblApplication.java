8712a65608bb8114fccd82423b917af8864182d7
Adrian Bala <Adrian Bala>
2017-05-18 07:01:52 +0200

    Initial commit

---
+++ src/main/java/pl/abala/prtbl/PrtblApplication.java
@@ -0,0 +1,12 @@
+package pl.abala.prtbl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrtblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrtblApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/abala/prtbl/PrtblApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.abala.prtbl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrtblApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
