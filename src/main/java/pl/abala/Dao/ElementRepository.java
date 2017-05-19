dd98d11e2d46282ecb8eab6eb11b39a418b964e8
AdB <adrian.bala@gft.com>
2017-05-19 22:43:28 +0200

    Removed comment

--- src/main/java/pl/abala/Dao/ElementRepository.java
+++ src/main/java/pl/abala/Dao/ElementRepository.java
@@ -8,9 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by AdB on 23.04.2017.
- */
 @Repository
 public class ElementRepository implements ElementDao {
     private static Map<Integer, Element> periodicTable;
@@ -21,34 +18,35 @@ public class ElementRepository implements ElementDao {
                 put(1, new Element(1, 1.00794, "H", "Hydrogen"));
                 put(2, new Element(2, 4.0026, "He", "Helium"));
                 put(3, new Element(3, 6.941, "Li", "Lithium"));
-				put(5, new Element(5, 10.811, "B", "Boron"));
-				put(6, new Element(6, 12.011, "C", "Carbon"));
-				put(7, new Element(7, 14.0067, "N", "Nitrogen"));
-				put(8, new Element(8, 15.9994, "O", "Oxygen"));
-				put(9, new Element(9, 18.9984, "F", "Fluorine"));
-				put(10, new Element(10, 20.1797, "Ne", "Neon"));
+                put(5, new Element(5, 10.811, "B", "Boron"));
+                put(6, new Element(6, 12.011, "C", "Carbon"));
+                put(7, new Element(7, 14.0067, "N", "Nitrogen"));
+                put(8, new Element(8, 15.9994, "O", "Oxygen"));
+                put(9, new Element(9, 18.9984, "F", "Fluorine"));
+                put(10, new Element(10, 20.1797, "Ne", "Neon"));
+                put(11, new Element(11, 22.98977, "Na", "Sodium"));
             }
         };
     }
 
     @Override
     public Collection<Element> getPeriodicTable() {
-        return this.periodicTable.values();
+        return periodicTable.values();
     }
 
     @Override
     public Element getElementByAtomicNumber(int atomicNumber) {
-        return this.periodicTable.get(atomicNumber);
+        return periodicTable.get(atomicNumber);
     }
 
     @Override
     public void delElementByAtomicNumber(int atomicNumber) {
-        this.periodicTable.remove(atomicNumber);
+        periodicTable.remove(atomicNumber);
     }
 
     @Override
     public void updElement(Element element) {
-        Element e = this.periodicTable.get(element.getAtomicNumber());
+        Element e = periodicTable.get(element.getAtomicNumber());
         e.setAtomicMass(element.getAtomicMass());
         e.setSymbol(element.getSymbol());
         e.setName(element.getName());
@@ -56,6 +54,6 @@ public class ElementRepository implements ElementDao {
 
     @Override
     public void addElement(Element element) {
-        this.periodicTable.put(element.getAtomicNumber(), element);
+        periodicTable.put(element.getAtomicNumber(), element);
     }
 }
