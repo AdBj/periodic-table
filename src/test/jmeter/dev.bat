b88fdc08e3cd43302a7e1f64ddfe24f01751c649
AdB <adrian.bala@gft.com>
2017-05-20 12:23:42 +0200

    Adjusted \ to Linux style /

--- src/test/jmeter/dev.bat
+++ src/test/jmeter/dev.bat
@@ -1,1 +1,1 @@
-".\apache-jmeter-3.2\bin\jmeter" -n -t ".\prtbl.jmx" -l "..\..\..\target\jmeter-log\prtbl.log" -e -o "..\..\..\target\jmeter-output"
+"./src/test/jmeter/apache-jmeter-3.2/bin/jmeter" -n -t "./prtbl.jmx" -l "../../../target/jmeter-log/prtbl.log" -e -o "../../../target/jmeter-output"
