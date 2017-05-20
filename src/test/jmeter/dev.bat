fd48697e2b50cd80b18dc27e9fa84a7481a10744
AdB <adrian.bala@gft.com>
2017-05-20 02:31:28 +0200

    Adjusted .gitignore

--- src/test/jmeter/dev.bat
+++ src/test/jmeter/dev.bat
@@ -1,1 +1,1 @@
-jmeter -n -t prtbl.jmx -l "..\..\..\target\jmeter-log\prtbl.log" -e -o "..\..\..\target\jmeter-output"
+$1 -n -t prtbl.jmx -l "..\..\..\target\jmeter-log\prtbl.log" -e -o "..\..\..\target\jmeter-output"
