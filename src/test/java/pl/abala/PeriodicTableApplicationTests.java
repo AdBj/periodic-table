4a6d47f8548cbc3cf5ead67639c1016c94a99192
AdB <adrian.bala@gft.com>
2017-05-28 20:16:47 +0200

    Refactored jUnit tests

--- src/test/java/pl/abala/PeriodicTableApplicationTests.java
+++ src/test/java/pl/abala/PeriodicTableApplicationTests.java
@@ -2,13 +2,35 @@ package pl.abala;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import pl.abala.controller.HomeController;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PeriodicTableApplicationTests {
-	@Test
-	public void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private HomeController controller;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello TestingCup 2017");
+    }
 }
